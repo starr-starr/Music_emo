39a3e37e82e719df230f6a37cf3265d15d842c1f
starr-starr <2654109566@qq.com>
2023-06-17 00:26:16 +0800

    ✨ add proxy configuration

--- vite.config.ts
+++ vite.config.ts
@@ -9,5 +9,14 @@ export default defineConfig({
       '@': join(__dirname, "src"),
     }
   },
+  server:{
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
   plugins: [react()],
 })
