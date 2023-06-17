766de0dc2533d0d7eb98b5122bc6b4feb5ca3284
starr-starr <2654109566@qq.com>
2023-06-17 13:09:35 +0800

    ✨ add Suspense

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from "react";
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from "react-router-dom";
 import App from '@/App.tsx'
@@ -5,7 +6,9 @@ import App from '@/App.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
+    <Suspense fallback={<div>loading...</div>}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Suspense>
 )
