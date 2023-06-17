14a9be6c4bfb349ea0a717e31bacfb25cdfcddd6
starr-starr <2654109566@qq.com>
2023-06-17 15:02:31 +0800

    ✨ add Outlet

--- src/pages/discover/index.tsx
+++ src/pages/discover/index.tsx
@@ -1,12 +1,16 @@
 import { memo }  from "react";
 import type { FC,ReactNode } from "react";
+import {Outlet} from "react-router-dom";
 
 interface MyProps {
     children? : ReactNode
 }
 const Discover: FC<MyProps> = memo(() => {
     return(
-        <div>Discover</div>
+        <>
+            <div>Discover</div>
+            <Outlet/>
+        </>
     )
 })
 
