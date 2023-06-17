4e7eb84cfb2b720edd979eebdd3c63f4397b90ae
starr-starr <2654109566@qq.com>
2023-06-17 23:35:39 +0800

    ✨ import normalize.css

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import App from '@/App.tsx'
 
 import { rootStore } from "@/store";
 
+import 'normalize.css'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
