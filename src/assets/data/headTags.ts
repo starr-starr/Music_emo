e0e972d3ebc4364eefd6c401e920872b6c9d3e0c
starr-starr <2654109566@qq.com>
2023-06-18 14:34:07 +0800

    📝 change key

--- src/assets/data/headTags.ts
+++ src/assets/data/headTags.ts
@@ -1,37 +1,37 @@
 export const headTags = [
   {
-    "title": "发现音乐",
-    "type": "path",
-    "path": "/discover"
+    title: "发现音乐",
+    type: "path",
+    path: "/discover"
   },
   {
-    "title": "我的音乐",
-    "type": "path",
-    "path": "/my"
+    title: "我的音乐",
+    type: "path",
+    path: "/my"
   },
   {
-    "title": "关注",
-    "type": "path",
-    "path": "/friend"
+    title: "关注",
+    type: "path",
+    path: "/friend"
   },
   {
-    "title": "商城",
-    "type": "link",
-    "link": "https://music.163.com/store/product"
+    title: "商城",
+    type: "link",
+    link: "https://music.163.com/store/product"
   },
   {
-    "title": "音乐人",
-    "type": "link",
-    "link": "https://music.163.com/st/musician"
+    title: "音乐人",
+    type: "link",
+    link: "https://music.163.com/st/musician"
   },
   {
-    "title": "云推歌",
-    "type": "link",
-    "link": "https://music.163.com/st/ad-song"
+    title: "云推歌",
+    type: "link",
+    link: "https://music.163.com/st/ad-song"
   },
   {
-    "title": "下载客户端",
-    "type": "path",
-    "path": "/download"
+    title: "下载客户端",
+    type: "path",
+    path: "/download"
   }
 ]
