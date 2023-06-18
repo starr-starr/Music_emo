dbb7143afe1a2358f6df96c501b23a7523f1d73d
starr-starr <2654109566@qq.com>
2023-06-18 16:52:11 +0800

    ✨ complete footer

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -8,7 +8,7 @@ interface MyProps {
 }
 const Footer: FC<MyProps> = memo(() => {
     return(
-        <div className='h-[172px] bg-[#f2f2f2] text-[#666] border-t-[#d3d3d3] border-t border-solid;'>
+        <div className='bg-[#f2f2f2] text-[#666] border-t-[#d3d3d3] border-t border-solid;'>
             <div className="flex justify-between items-center w-[980px] my-0 mx-auto flex-col">
                 <div className="flex mt-[30px] footItem">
                     {
@@ -22,36 +22,26 @@ const Footer: FC<MyProps> = memo(() => {
                         })
                     }
                 </div>
-                {/*<div className="leading-6 pt-[15px]">*/}
-                {/*        {*/}
-                {/*            footerLinks.map(item => {*/}
-                {/*                return (*/}
-                {/*                    <div key={item.title} className='text-[#D9D9D9]'>*/}
-                {/*                        <a href={item.link} target="_blank" rel="noopener noreferrer">{item.title}</a>*/}
-                {/*                        <span className="my-0 mx-[8px]">|</span>*/}
-                {/*                    </div>*/}
-                {/*                )*/}
-                {/*            })*/}
-                {/*        }*/}
-                {/*    <div>*/}
-                {/*        <span className='mr-[15px]'>网易公司版权所有©1997-2020</span>*/}
-                {/*        <span className='mr-[15px]'>杭州乐读科技有限公司运营：*/}
-                {/*            <a href="https://p1.music.126.net/Mos9LTpl6kYt6YTutA6gjg==/109951164248627501.png" rel="noopener noreferrer" target="_blank">浙网文[2018]3506-263号</a>*/}
-                {/*        </span>*/}
-                {/*    </div>*/}
-                {/*    <div>*/}
-                {/*        <span>违法和不良信息举报电话：0571-89853516</span>*/}
-                {/*        <span>举报邮箱：*/}
-                {/*            <a href="mailto:ncm5990@163.com" target="_blank" rel="noopener noreferrer">ncm5990@163.com</a>*/}
-                {/*        </span>*/}
-                {/*    </div>*/}
-                {/*    <div>*/}
-                {/*        <span>粤B2-20090191-18</span>*/}
-                {/*        <a href="http://www.beian.miit.gov.cn/publish/query/indexFirst.action" rel="noopener noreferrer" target="_blank">*/}
-                {/*            工业和信息化部备案管理系统网站*/}
-                {/*        </a>*/}
-                {/*    </div>*/}
-                {/*</div>*/}
+                <div className="leading-6 pt-[15px] text-[12px]">
+                    <div className='flex mt-[20px]'>
+                        {
+                            footerLinks.map((item,index) => {
+                                return (
+                                    <div key={item.title} className='text-[#333]'>
+                                        <a href={item.link} target="_blank" rel="noopener noreferrer">{item.title}</a>
+                                        { index < 6 && <span className="my-0 mx-[8px]">|</span>}
+                                    </div>
+                                )
+                            })
+                        }
+                    </div>
+                    <div>
+                        <span className='mr-[15px]'>网易公司版权所有©1997-2023</span>
+                        <span className='mr-[15px]'>杭州乐读科技有限公司运营：
+                            <a href="https://p1.music.126.net/Mos9LTpl6kYt6YTutA6gjg==/109951164248627501.png" rel="noopener noreferrer" target="_blank">浙网文[2021] 1186-054号</a>
+                        </span>
+                    </div>
+                </div>
             </div>
         </div>
     )
