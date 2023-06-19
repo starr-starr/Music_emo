5dcc76d3502bab6d75f28f5fda44072e13f363bb
starr-starr <2654109566@qq.com>
2023-06-19 15:26:21 +0800

    ✨ complete left swiper

--- src/pages/discover/c-pages/recommend/c-cpns/swiper/index.tsx
+++ src/pages/discover/c-pages/recommend/c-cpns/swiper/index.tsx
@@ -1,19 +1,73 @@
-import { memo } from "react";
-import type { FC,ReactNode } from "react";
-import {useGetBannerDataQuery} from "@/store/api/discover.ts";
+import { memo, useRef, useState} from "react";
+import type { FC,ReactNode,ElementRef } from "react";
+import { useGetBannerDataQuery } from "@/store/api/discover.ts";
 
-// import { Carousel } from "antd";
+import { Carousel } from "antd";
+
 
 interface MyProps {
     children? : ReactNode
 }
 const Swiper: FC<MyProps> = memo(() => {
-    // const { data } = useGetBannerDataQuery()
-    // console.log(data);
+    const { data } = useGetBannerDataQuery()
+    const [ selectIndex, setSelectIndex ] = useState(0)
+    const carouselRef = useRef<ElementRef<typeof Carousel>>(null)
+
+    let bannerImg = data && `${data[selectIndex]?.imageUrl}?imageView&blur=40x20`
+
+    function handleCarouselAfterChange(index:number){
+        setSelectIndex(index)
+    }
+
+    function handleLiClick(index:number){
+        carouselRef.current?.goTo(index);
+        setSelectIndex(index)
+    }
+
     return(
-        <>
-            Swiper
-        </>
+        <div
+            style={{background:`url('${bannerImg}') center center / 6000px`}}
+        >
+            <div className='w-[982px] h-[270px] my-0 mx-auto flex relative'>
+                <div className='relative w-[730px]'>
+                    <Carousel
+                        autoplay
+                        dots={false}
+                        autoplaySpeed={10000}
+                        effect="fade"
+                        ref={carouselRef}
+                        afterChange={handleCarouselAfterChange}
+                    >
+                        {
+                            data && data.map((item) => {
+                                return (
+                                    <div className="h-[275px] truncate" key={item.imageUrl}>
+                                            <img
+                                                className="w-full"
+                                                src={item.imageUrl}
+                                                alt={item.typeTitle}
+                                            />
+                                    </div>
+                                )
+                            })
+                        }
+                    </Carousel>
+                    <ul className="absolute flex justify-center mx-auto my-0 bottom-0 inset-x-0">
+                        {
+                            data && data.map((item,index) => {
+                                return (
+                                    <li className='my-0 mx-[2px] bannerLi' key={item.imageUrl} onClick={()=>handleLiClick(index)}>
+                                        <span
+                                            className={`bannerItem inline-block w-[20px] h-[20px] cursor-pointer z-[9] ${ index === selectIndex ? 'active' : 'noactive'}`}>
+                                        </span>
+                                    </li>
+                                )
+                            })
+                        }
+                    </ul>
+                </div>
+            </div>
+        </div>
     )
 })
 
