e2e6065f1db4989e6055875cf862ba3117e85609
starr-starr <2654109566@qq.com>
2023-06-19 12:38:13 +0800

    🔥 remove useDispatch & useSelector & shallowEqual

--- src/store/hooks.ts
+++
@@ -1,8 +0,0 @@
-import {TypedUseSelectorHook, useDispatch, useSelector, shallowEqual} from 'react-redux'
-import type {RootState, AppDispatch} from '@/store'
-
-// 在整个应用程序中使用，而不是简单的 `useDispatch` 和 `useSelector`
-export const useAppDispatch: () => AppDispatch = useDispatch
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
-//  防止使用 shallowEqual 时重新从 react-redux 导入
-export const shallowEqualApp = shallowEqual
--- src/store/index.ts
+++ src/store/index.ts
@@ -27,6 +27,5 @@ setupListeners(rootStore.dispatch);
 
 // 从 store 本身推断出 `RootState` 和 `AppDispatch` 类型
 export type RootState = ReturnType<typeof rootStore.getState>;
-// 推断出类型: {posts: PostsState, comments: CommentsState, users: UsersState}
-export type AppDispatch = typeof rootStore.dispatch
+
 
