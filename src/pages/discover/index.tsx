279b57addaf05e048d83ba95ad5bb389ca806fc7
starr-starr <2654109566@qq.com>
2023-06-20 11:26:03 +0800

    🎨 add space

--- src/pages/discover/index.tsx
+++ src/pages/discover/index.tsx
@@ -1,6 +1,6 @@
 import { memo }  from "react";
 import type { FC,ReactNode } from "react";
-import {Outlet} from "react-router-dom";
+import { Outlet } from "react-router-dom";
 
 import SubNav from "@/pages/discover/c-cpns/subNav";
 
