dde0b8820b80bc372fba84dc31c9184673847503
starr-starr <2654109566@qq.com>
2023-06-21 14:38:17 +0800

    ✨ add hotRecommendType & getHotRecommendData

--- src/pages/discover/c-pages/recommend/c-cpns/hotRecommend/index.tsx
+++ src/pages/discover/c-pages/recommend/c-cpns/hotRecommend/index.tsx
@@ -1,14 +1,17 @@
 import { memo }  from "react";
 import type { FC,ReactNode } from "react";
 import MusicListHeader from "@/components/musicListHeader";
+import { useGetHotRecommendDataQuery } from "@/store/api/discover.ts";
 
 interface MyProps {
     children? : ReactNode
 }
 const HotRecommend: FC<MyProps> = memo(() => {
+    const { data } = useGetHotRecommendDataQuery(30)
     return(
         <>
             <MusicListHeader  title="热门推荐" keyword={["华语","流行","摇滚","民谣","电子"]} linkplace="/discover/playlist"/>
+
         </>
     )
 })
--- src/store/api/discover.ts
+++ src/store/api/discover.ts
@@ -4,18 +4,30 @@ interface bannerType {
     banners: any[]
 }
 
+interface hotRecommendType {
+    result : any[]
+}
+
 const discoverApi = baseApi.injectEndpoints({
     endpoints:( builder ) => ({
+        //  query<a,b>  a 是返回值的类型，b 是传给后端的类型
         getBannerData: builder.query<bannerType['banners'], void>({
             query: () => '/banner',
             //  响应拦截器
             transformResponse(response: { banners: bannerType['banners'] }) {
                 return response.banners
             }
+        }),
+        getHotRecommendData: builder.query<hotRecommendType['result'], number>({
+            query: ( limit:number ) => `/personalized/${limit}`,
+            transformResponse(response: { result: hotRecommendType['result'] } ) {
+                return response.result
+            }
         })
     })
 })
 
 export const {
-    useGetBannerDataQuery
+    useGetBannerDataQuery,
+    useGetHotRecommendDataQuery
 } = discoverApi
