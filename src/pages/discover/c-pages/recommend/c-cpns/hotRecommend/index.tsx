c05e63ab2b706cffbb40cc173f5407fe52968575
starr-starr <2654109566@qq.com>
2023-06-21 15:49:53 +0800

    💩 add HotRecommend error handle

--- src/pages/discover/c-pages/recommend/c-cpns/hotRecommend/index.tsx
+++ src/pages/discover/c-pages/recommend/c-cpns/hotRecommend/index.tsx
@@ -3,15 +3,13 @@ import type { FC,ReactNode } from "react";
 import MusicListHeader from "@/components/musicListHeader";
 import { useGetHotRecommendDataQuery } from "@/store/api/discover.ts";
 import MusicListItem from "@/components/MusicListItem";
+import { useRenderSuccess } from "@/hooks/useRenderSuccessData.tsx";
 
 interface MyProps {
     children? : ReactNode
 }
-const HotRecommend: FC<MyProps> = memo(() => {
-    const { data,isSuccess } = useGetHotRecommendDataQuery(30)
-    console.log(isSuccess);
-
-    return(
+const HotRecommendItem = ( { data }: { data:any[] } ) => {
+    return (
         <>
             <MusicListHeader  title="热门推荐" keyword={["华语","流行","摇滚","民谣","电子"]} linkplace="/discover/playlist"/>
             {
@@ -19,6 +17,17 @@ const HotRecommend: FC<MyProps> = memo(() => {
                     return <MusicListItem key={item.id} itemData={item} />
                 })
             }
+        </>
+    )
+}
+
+const HotRecommend: FC<MyProps> = memo(() => {
+    const { data,isSuccess } = useGetHotRecommendDataQuery(30)
+    console.log(isSuccess);
+
+    return(
+        <>
+            {useRenderSuccess(isSuccess,data && <HotRecommendItem data={data}/>)}
         </>
     )
 })
