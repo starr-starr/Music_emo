f01b00d848519fdefea0ac482d501992148590a6
starr-starr <2654109566@qq.com>
2023-06-21 15:02:36 +0800

    ✨ add MusicListItem to HotRecommend

--- src/pages/discover/c-pages/recommend/c-cpns/hotRecommend/index.tsx
+++ src/pages/discover/c-pages/recommend/c-cpns/hotRecommend/index.tsx
@@ -2,16 +2,23 @@ import { memo }  from "react";
 import type { FC,ReactNode } from "react";
 import MusicListHeader from "@/components/musicListHeader";
 import { useGetHotRecommendDataQuery } from "@/store/api/discover.ts";
+import MusicListItem from "@/components/MusicListItem";
 
 interface MyProps {
     children? : ReactNode
 }
 const HotRecommend: FC<MyProps> = memo(() => {
-    const { data } = useGetHotRecommendDataQuery(30)
+    const { data,isSuccess } = useGetHotRecommendDataQuery(30)
+    console.log(isSuccess);
+
     return(
         <>
             <MusicListHeader  title="热门推荐" keyword={["华语","流行","摇滚","民谣","电子"]} linkplace="/discover/playlist"/>
-
+            {
+                data && data.map( item => {
+                    return <MusicListItem key={item.id} itemData={item} />
+                })
+            }
         </>
     )
 })
