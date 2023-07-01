ef64a40c9f0d4803bca394c117967315ae7ccd43
starr-starr <2654109566@qq.com>
2023-07-01 22:01:03 +0800

    ✨ add newAlum

--- src/pages/discover/c-pages/recommend/index.tsx
+++ src/pages/discover/c-pages/recommend/index.tsx
@@ -3,6 +3,7 @@ import type { FC,ReactNode } from "react";
 
 import Swiper from "@/pages/discover/c-pages/recommend/c-cpns/swiper";
 import HotRecommend from "@/pages/discover/c-pages/recommend/c-cpns/hotRecommend";
+import NewAlbum from "@/pages/discover/c-pages/recommend/c-cpns/newAlbum";
 
 interface MyProps {
     children? : ReactNode
@@ -14,6 +15,7 @@ const Recommend: FC<MyProps> = memo(() => {
             <div className={`w-[980px] mx-auto my-0 border bg-[url(${('@/assets/img/wrap-bg.png')}] flex border-solid border-[#d3d3d3]`}>
                 <div className='p-[20px] w-[729px]'>
                     <HotRecommend/>
+                    <NewAlbum/>
                 </div>
             </div>
         </>
