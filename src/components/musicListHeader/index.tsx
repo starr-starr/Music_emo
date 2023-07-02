6129db96bb59261d49240f874900d4c81ea2e91d
starr-starr <2654109566@qq.com>
2023-07-02 23:14:07 +0800

    ♻️ move icon to only div

--- src/components/musicListHeader/index.tsx
+++ src/components/musicListHeader/index.tsx
@@ -16,22 +16,27 @@ const MusicListHeader: FC<MyProps> = memo((props) => {
 
     return(
         <div
-            style={{backgroundPosition:'-225px -156px'}}
-            className='decorateBtn h-[33px] flex justify-between items-center pl-[34px] pr-2.5 pt-0 pb-1 border-b-2 border-b-[#c10d0c] border-solid'
+            className='decorateBtn  h-[33px] flex justify-between items-center  pr-2.5 pt-0 pb-1 border-b-2 border-b-[#c10d0c] border-solid'
         >
-            <div className="flex items-center">
-                <Link to={linkplace} className="font-[normal] text-xl leading-7 mr-5">{title}</Link>
+            <div className="flex">
+                <div
+                    style={{backgroundPosition:'-225px -156px'}}
+                    className="decorateBtn flex w-[25px] h-[25px]"
+                ></div>
                 <div className="flex items-center">
-                    {
-                        keyword?.map((item)=>{
-                            return(
-                                <div className="relative top-[2px] text-[#666] text-[12px]" key={item}>
-                                    <span className="cursor-pointer hover:underline">{item}</span>
-                                    { ( item !== keyword.at(-1) ) && <span className='my-0 mx-[10px] '>|</span>}
-                                </div>
-                            )
-                        })
-                    }
+                    <Link to={linkplace} className="font-[normal] text-xl leading-7 mr-5">{title}</Link>
+                    <div className="flex items-center">
+                        {
+                            keyword?.map((item)=>{
+                                return(
+                                    <div className="relative top-[2px] text-[#666] text-[12px]" key={item}>
+                                        <span className="cursor-pointer hover:underline">{item}</span>
+                                        { ( item !== keyword.at(-1) ) && <span className='my-0 mx-[10px] '>|</span>}
+                                    </div>
+                                )
+                            })
+                        }
+                    </div>
                 </div>
             </div>
             <div className="flex items-center">
