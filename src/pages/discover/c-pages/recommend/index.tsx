50af4d23f0b2dcc366c3b3ac0593b88e753540d4
starr-starr <2654109566@qq.com>
2023-07-02 17:55:06 +0800

    ✨ add SignedSinger component

--- src/pages/discover/c-pages/recommend/index.tsx
+++ src/pages/discover/c-pages/recommend/index.tsx
@@ -5,6 +5,7 @@ import Swiper from "@/pages/discover/c-pages/recommend/c-cpns/swiper";
 import HotRecommend from "@/pages/discover/c-pages/recommend/c-cpns/hotRecommend";
 import NewAlbum from "@/pages/discover/c-pages/recommend/c-cpns/newAlbum";
 import UserLogin from "@/pages/discover/c-pages/recommend/c-cpns/userLogin";
+import SignedSinger from "@/pages/discover/c-pages/recommend/c-cpns/signedSinger";
 
 interface MyProps {
     children? : ReactNode
@@ -20,6 +21,7 @@ const Recommend: FC<MyProps> = memo(() => {
                 </div>
                 <div className='ml-[1px] w-[250px]'>
                     <UserLogin/>
+                    <SignedSinger/>
                 </div>
             </div>
         </>
