b6878a70f227650b5d3f153b98e56892c5430978
starr-starr <2654109566@qq.com>
2023-07-02 15:19:16 +0800

    ✨ add userLogin component

--- src/pages/discover/c-pages/recommend/index.tsx
+++ src/pages/discover/c-pages/recommend/index.tsx
@@ -4,6 +4,7 @@ import type { FC,ReactNode } from "react";
 import Swiper from "@/pages/discover/c-pages/recommend/c-cpns/swiper";
 import HotRecommend from "@/pages/discover/c-pages/recommend/c-cpns/hotRecommend";
 import NewAlbum from "@/pages/discover/c-pages/recommend/c-cpns/newAlbum";
+import UserLogin from "@/pages/discover/c-pages/recommend/c-cpns/userLogin";
 
 interface MyProps {
     children? : ReactNode
@@ -16,6 +17,9 @@ const Recommend: FC<MyProps> = memo(() => {
                 <div className='p-[20px] w-[729px]'>
                     <HotRecommend/>
                     <NewAlbum/>
+                </div>
+                <div className='ml-[1px] w-[250px]'>
+                    <UserLogin/>
                 </div>
             </div>
         </>
