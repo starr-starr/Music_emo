0c41c0a8ed97713128e641ad0389e92867a82ce5
starr-starr <2654109566@qq.com>
2023-07-08 03:36:51 +0800

    👽️ change backend url

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
     port:3001,
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
+        target: "https://netease-cloud-music-api-nine-orcin-25.vercel.app/",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
