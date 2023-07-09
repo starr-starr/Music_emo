e3845715a55ec69b1e5df47d3d6bd5ff59a72334
starr-starr <2654109566@qq.com>
2023-07-10 02:09:10 +0800

    ✨ add useGetSongLyricDataQuery

--- src/store/api/play/playApi.ts
+++ src/store/api/play/playApi.ts
@@ -2,21 +2,28 @@ import { baseApi } from "@/store/api/base.ts";
 
 interface songDetailType {
     songs: any[]
-    privileges: any[]
 }
-type songDetailResponse = songDetailType[]
-
+interface LyricType {
+    lrc: any
+}
 const playApi = baseApi.injectEndpoints({
     endpoints:(build)=>({
-        getSongDetailData: build.query<songDetailResponse,number>({
+        getSongDetailData: build.query<songDetailType['songs'],number>({
             query: ( ids:number ) => `/song/detail?ids=${ids}`,
             transformResponse(response: { songs: songDetailType['songs'] } ) {
                 return response.songs
+            }
+        }),
+        getSongLyricData: build.query<LyricType['lrc'],number>({
+            query: ( id:number ) => `/lyric?id=${id}`,
+            transformResponse(response: { lrc: LyricType['lrc'] } ) {
+                return response.lrc.lyric
             }
         }),
     })
 })
 
 export const {
-    useGetSongDetailDataQuery
+    useGetSongDetailDataQuery,
+    useGetSongLyricDataQuery
 } = playApi
