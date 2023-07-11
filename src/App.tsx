6aac1b6ff961b1d882d918581d5c6becf33c5276
starr-starr <2654109566@qq.com>
2023-07-12 01:33:38 +0800

    ✨ add FetchCurrentSong in App

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,20 @@ import {routes} from "@/route/route.tsx";
 import Header from "@/components/header";
 import Footer from "@/components/footer";
 import Player from "@/components/player";
+import {useThunkDispatch} from "@/store/hooks.ts";
+import {FetchCurrentSong} from "@/store/api/play/store.ts";
+import {useEffect} from "react";
+import {useGetSongDetailDataQuery, useGetSongLyricDataQuery} from "@/store/api/play/playApi.ts";
 
 function App() {
-
+    const { data: songDetailData } = useGetSongDetailDataQuery(4877413)
+    const { data: songLyricData } = useGetSongLyricDataQuery(4877413)
+    const dispatch = useThunkDispatch()
+    useEffect(() => {
+        if ( songDetailData &&  songLyricData){
+            dispatch(FetchCurrentSong({songId:4877413,songDetailData,songLyricData}))
+        }
+    }, [])
     return (
         <>
             <Header/>
