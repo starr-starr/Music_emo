c0c2f762305942d8d867d3503c825d8c34241447
starr-starr <2654109566@qq.com>
2023-07-12 02:01:59 +0800

    ✨ add useGetSongUrlDataQuery

--- src/store/api/play/playApi.ts
+++ src/store/api/play/playApi.ts
@@ -3,8 +3,11 @@ import { baseApi } from "@/store/api/base.ts";
 interface songDetailType {
     songs: any[]
 }
-interface LyricType {
+interface lyricType {
     lrc: any
+}
+interface SongUrlType {
+    data: any
 }
 const playApi = baseApi.injectEndpoints({
     endpoints:(build)=>({
@@ -14,10 +17,16 @@ const playApi = baseApi.injectEndpoints({
                 return response.songs
             }
         }),
-        getSongLyricData: build.query<LyricType['lrc'],number>({
+        getSongLyricData: build.query<lyricType['lrc'],number>({
             query: ( id:number ) => `/lyric?id=${id}`,
-            transformResponse(response: { lrc: LyricType['lrc'] } ) {
+            transformResponse(response: { lrc: lyricType['lrc'] } ) {
                 return response.lrc.lyric
+            }
+        }),
+        getSongUrlData: build.query<SongUrlType['data'],number>({
+            query: ( id:number ) => `/song/url?id=${id}`,
+            transformResponse(response: { data: SongUrlType['data'] } ) {
+                return response.data[0].url
             }
         }),
     })
@@ -25,5 +34,6 @@ const playApi = baseApi.injectEndpoints({
 
 export const {
     useGetSongDetailDataQuery,
-    useGetSongLyricDataQuery
+    useGetSongLyricDataQuery,
+    useGetSongUrlDataQuery
 } = playApi
