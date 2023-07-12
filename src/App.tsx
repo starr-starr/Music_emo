52ae927c036ca4e46ec14114a30dea9042ad52e2
starr-starr <2654109566@qq.com>
2023-07-13 02:07:34 +0800

    ✨ add initial fn to add music

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,25 @@ import {routes} from "@/route/route.tsx";
 import Header from "@/components/header";
 import Footer from "@/components/footer";
 import Player from "@/components/player";
-import {useFetchSongData} from "@/hooks/useFetchSongData.tsx";
+import {useEffect} from "react";
+import {apiGetSongDetail, apiGetSongLyric} from "@/store/api/play/fetchService.ts";
+import {FetchCurrentSong} from "@/store/api/play/store.ts";
+import {useThunkDispatch} from "@/store/hooks.ts";
 
 function App() {
-    useFetchSongData(2049512697)
+    const dispatch = useThunkDispatch()
+    useEffect(()=>{
+        const fn = async (songId:number) =>{
+            const songDetail = await apiGetSongDetail(songId)
+            const songLyric = await apiGetSongLyric(songId)
+            dispatch(FetchCurrentSong({
+                songId,
+                songDetailData:songDetail[0],
+                songLyricData:songLyric }
+            ))
+        }
+        fn(2049512697)
+    })
     return (
         <>
             <Header/>
